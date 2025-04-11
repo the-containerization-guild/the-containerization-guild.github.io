5abfd4730ccb3300733f469f4b396849a01c3396
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-04-11 15:07:53 +0100

    clean up welcome section
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,44 +1,23 @@
+import styles from './styles.module.css'
+
 export default function Welcome() {
-    return (
-        <div className="welcome viewport welcome-grid">
-          <div className="welcome-info">
-            <h1 className="header">Name of project</h1>
-            <h2 className="welcome-h2">
-              Flavour text for project
-            </h2>
-            <ul>
-              <li>
-                  <button className="static-button" href="#">
-                    <img alt="Button 1"></img>
-                    Link 1
-                  </button>
-              </li>
-              <li>
-                  <button className="static-button" href="#">
-                    <img alt="Button 2"></img>
-                    Link 2
-                  </button>
-              </li>
-              <li>
-                  <button className="static-button" href="#">
-                    <img alt="Button 3"></img>
-                    Link 3
-                  </button>
-              </li>
-              <li>
-                  <button className="static-button" href="#">
-                    <img alt="Button 4"></img>
-                    Link 4
-                  </button>
-              </li>
-            </ul>
-          </div>
-          <div className="welcome-image">
-            <img
-              className="logo-full"
-              alt=""
-            ></img>
-          </div>
-        </div>
-    )
+  return (
+    <div className="welcome viewport">
+      <div className="welcome-info">
+        <h1 className="header">Welcome to the Guild</h1>
+        <ul>
+          <li>
+            <button className="static-button" href="#">
+              Join a gathering
+            </button>
+          </li>
+          <li>
+            <button className="static-button" href="#">
+              Watch previous gatherings
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
