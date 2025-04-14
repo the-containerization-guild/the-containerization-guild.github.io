aa6f709d82bcdfd5c33d309f404be4ab4248a306
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-04-14 10:49:04 +0100

    fix buttons
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,22 +1,18 @@
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 export default function Welcome() {
   return (
     <div className="welcome viewport">
       <div className="welcome-info">
         <h1 className="header">Welcome to the Guild</h1>
-        <ul>
-          <li>
-            <button className="static-button" href="#">
-              Join a gathering
-            </button>
-          </li>
-          <li>
-            <button className="static-button" href="#">
-              Watch previous gatherings
-            </button>
-          </li>
-        </ul>
+        <div className="button-group">
+          <button className="static-button" href="#">
+            Join a gathering
+          </button>
+          <button className="static-button" href="#">
+            Watch previous gatherings
+          </button>
+        </div>
       </div>
     </div>
   );
