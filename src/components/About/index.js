cdd2c114f03ded58ffd5ae102afd9343e6ec5535
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-04-23 16:48:48 +0100

    edit about section with new content
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,56 +1,43 @@
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 export default function About() {
   return (
-      <div className="about about-viewport" id="about">
-        <div className="about-info">
-          <h1 className="about-header">About header here</h1>
-          <div className="about-grid">
-            <img
-              className="about-graphic-1"
-              alt="About Graphic 1"
-            ></img>
-            <p className="about-text-1" role="paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
-              odio pretium leo porta lobortis. Sed et sem scelerisque, fermentum
-              lacus et, dapibus tortor. Nam consectetur ultricies urna id
-              fringilla.
-            </p>
+    <div className="about about-viewport" id="about">
+      <div className="about-info">
+        <h1 className="about-header">What happens at Gatherings?</h1>
 
-            <img
-              className="about-graphic-2"
-              alt="About Graphic 2"
-            ></img>
-            <p className="about-text-2" role="paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
-              odio pretium leo porta lobortis. Sed et sem scelerisque, fermentum
-              lacus et, dapibus tortor. Nam consectetur ultricies urna id
-              fringilla.
-            </p>
+        <div className="about-grid">
+          <p className="about-para" role="paragraph">
+            Gatherings are opportunities to share new and evolving ideas
+            pertaining to containerization. This is a safe space to explore what
+            could be and appreciate our shared love of engineering. All ideas
+            are welcome: from shower thoughts to manifestos to maybe the worst
+            idea ever. We are not here to judge.
+            <br/>
+            <br/>
+            Talks are capped at 10-12 minutes. Participants are expected to
+            encourage the speaker with emojis, positive feedback, suggestions
+            for next steps, and/or connections to likeminded folks. Gatherings
+            currently occur online each quarter. Each gathering is 2-3 hours
+            long. It flies by.
+          </p>
+          <img className="about-gif" alt="About Graphic 1"></img>
+        </div>
 
-            <img
-              className="about-graphic-3"
-              alt="About Graphic 3"
-            ></img>
-            <p className="about-text-3" role="paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
-              odio pretium leo porta lobortis. Sed et sem scelerisque, fermentum
-              lacus et, dapibus tortor. Nam consectetur ultricies urna id
-              fringilla.
-            </p>
+        <h1 className="about-header">About the Containerization Guild</h1>
 
-            <img
-              className="about-graphic-4"
-              alt="About Graphic 4"
-            ></img>
-            <p className="about-text-4" role="paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at
-              odio pretium leo porta lobortis. Sed et sem scelerisque, fermentum
-              lacus et, dapibus tortor. Nam consectetur ultricies urna id
-              fringilla.
-            </p>
-          </div>
+        <div className="about-grid">
+          <p className="about-para" role="paragraph">
+            The Containerization Guild was formed in response to rapidly
+            evolving infrastructure needs and capabilities. The technology to
+            deliver containerization, as a solution to meet those needs, needed
+            to evolve rapidly as well. We wanted a space where folks excited (or
+            very concerned!) about the possibilities could share ideas without
+            the burden of defending the ideas.
+          </p>
+          <img className="about-gif" alt="About Graphic 1"></img>
         </div>
       </div>
+    </div>
   );
 }
