fd7217e112c4e0a43fcb78b714fdd817538e45e7
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-05 12:20:21 +0100

    modify config again
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "the-containerization-guild", // Usually your GitHub org/user name.
   projectName: "website", // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
