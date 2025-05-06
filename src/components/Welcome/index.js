82c68cc66bbd995b73da1bd5673a9dced501c26b
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-06 13:34:35 +0100

    update buttons on the welcome page with links and new hover colours
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -6,8 +6,11 @@ export default function Welcome() {
       <div className="welcome-info">
         <h1 className="header">Welcome to the Guild</h1>
         <div className="button-group">
-          <button className="static-button" href="#">
-            Join next gathering
+          <button className="static-button-bluesky" href="https://bsky.app/profile/container-guild.bsky.social">
+            Follow us on BlueSky
+          </button>
+          <button className="static-button-discord" href="https://discord.gg/aJdVtxEm">
+            Join our Discord
           </button>
           <button className="static-button" href="#">
             Watch previous gatherings
@@ -16,4 +19,4 @@ export default function Welcome() {
       </div>
     </div>
   );
-}
+};
