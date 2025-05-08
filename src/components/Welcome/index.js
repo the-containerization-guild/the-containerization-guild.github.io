20caab82fdef1a9f66c182ef5247deaabfc1cd2b
Jess Chitas <36636982+jessicachitas@users.noreply.github.com>
2025-05-08 15:19:32 +0100

    add new logo for mobile version of website
    Signed-off-by: Jess Chitas <36636982+jessicachitas@users.noreply.github.com>

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,12 +1,14 @@
 import styles from "./styles.module.css";
-import logo from "../../../static/img/full-logo-2.png";
+import logo1 from "../../../static/img/full-logo-2.png";
+import logo2 from "../../../static/img/full-logo.png";
 
 export default function Welcome() {
   return (
     <div className="welcome viewport">
       <div className="welcome-info">
         <h1 className="header">Welcome to</h1>
-        <img className="welcome-logo" width="75%" src={logo}></img>
+        <img className="welcome-logo" width="75%" src={logo1}></img>
+        <img className="welcome-logo-small" width="100%" src={logo2}></img>
         <div className="button-group">
           <button className="static-button-bluesky" href="https://bsky.app/profile/container-guild.bsky.social">
             Follow us on BlueSky
